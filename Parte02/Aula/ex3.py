ce8a18838d9b450919b7693b146fee1cb1b72ead
GoncaloR00 <gribeiro@ua.pt>
2022-10-08 01:13:00 +0100

    teste

--- Parte02/Aula/ex3.py
+++ Parte02/Aula/ex3.py
@@ -6,11 +6,11 @@ import numpy as np
 scene = cv2.imread('images/scene.jpg')
 wally = cv2.imread('images/wally.png')
 h,w,_ = wally.shape
-method = cv2.TM_CCOEFF
+method =cv2.TM_CCOEFF
 result = cv2.matchTemplate(scene, wally, method)
 _,_,_,max_loc = cv2.minMaxLoc(result)
 min_loc = (max_loc[0]+h, max_loc[1]+w)
-scene_gray = cv2.COLOR_BGR2GRAY(scene)
+se_gray = cv2.COLOR_BGR2GRAY(scene)
 image_gui = scene * 0
 wally_loc = 
 cv2.imshow('resultado',wally.loc)
